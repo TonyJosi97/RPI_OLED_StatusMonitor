81bff44f8005a84a113b823a3f3cb767baaf5b74
Tony Josi <tonyjosinew@gmail.com>
2021-07-29 23:43:04 +0530

    initial

--- rpi_status_disp.py
+++ rpi_status_disp.py
@@ -1,2 +1,20 @@
 # Main file
 
+from luma.core.interface.serial import i2c
+from luma.core.interface.parallel import bitbang_6800
+from luma.core.render import canvas
+from luma.oled.device import sh1106
+
+# rev.1 users set port=0
+# substitute spi(device=0, port=0) below if using that interface
+# substitute bitbang_6800(RS=7, E=8, PINS=[25,24,23,27]) below if using that interface
+serial = i2c(port=1, address=0x3C)
+
+# substitute ssd1331(...) or sh1106(...) below if using that device
+device = sh1106(serial)
+
+with canvas(device) as draw:
+    draw.rectangle(device.bounding_box, outline="white", fill="black")
+    draw.text((30, 40), "Hello World", fill="white")
+
+
