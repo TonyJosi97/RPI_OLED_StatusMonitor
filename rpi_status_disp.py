0ebf82c311d22397f39deedac92a1ed163465497
Tony Josi <tonyjosinew@gmail.com>
2021-08-02 23:54:29 +0530

    added fan control from GPIO

--- rpi_status_disp.py
+++ rpi_status_disp.py
@@ -24,12 +24,13 @@ CPU_FAN_TURN_OFF_THRESH = 40
 OLED_LEFT_PADDING = 10
 
 ## Globals
-owm_manager = None
-oled_disp_sh1106 = None
-prev_weather_check_time = 0.0
-weather_data = None
-got_weather_data = False
+g_owm_manager = None
+g_oled_disp_sh1106 = None
+g_prev_weather_check_time = 0.0
+g_weather_data = None
+g_got_weather_data = False
 g_cur_temp = 0
+g_cpu_fan_on = False
 
 def get_temp(window = 20, samp_period_ms = 20):
     temp_sum = 0
@@ -107,9 +108,9 @@ def check_network_connection():
 def init_modules():
 
     # Init open weather map API
-    global owm_manager
+    global g_owm_manager
     owm = OWM(str(get_owm_authtoken()))
-    owm_manager = owm.weather_manager()
+    g_owm_manager = owm.weather_manager()
 
     # Init GPIO API
     GPIO.setmode(GPIO.BCM)                              # choose BCM GPIO address layout 
@@ -117,18 +118,18 @@ def init_modules():
 
 
 def init_device():
-    global oled_disp_sh1106
+    global g_oled_disp_sh1106
     # rev.1 users set port=0
     # substitute spi(device=0, port=0) below if using that interface
     # substitute bitbang_6800(RS=7, E=8, PINS=[25,24,23,27]) below if using that interface
     serial = i2c(port=1, address=0x3C)
 
     # substitute ssd1331(...) or sh1106(...) below if using that device
-    oled_disp_sh1106 = sh1106(serial)
+    g_oled_disp_sh1106 = sh1106(serial)
 
 def get_weather_data():
-    global owm_manager
-    observation = owm_manager.weather_at_place('Karukachal,IN')
+    global g_owm_manager
+    observation = g_owm_manager.weather_at_place('Karukachal,IN')
     w = observation.weather
     temptr = w.temperature('celsius')
     return [str(temptr.get("temp")), str(w.clouds), str(w.detailed_status), str(w.humidity), str(w.rain)]
@@ -147,9 +148,9 @@ def get_current_cpu_freq():
 
 def update_oled_screen():
 
-    global oled_disp_sh1106, prev_weather_check_time, weather_data, got_weather_data, g_cur_temp
+    global g_oled_disp_sh1106, g_prev_weather_check_time, g_weather_data, g_got_weather_data, g_cur_temp
 
-    with canvas(oled_disp_sh1106) as draw:
+    with canvas(g_oled_disp_sh1106) as draw:
         #draw.rectangle(device.bounding_box, outline="white", fill="black")
         #draw.text((5, 5), "TONY'S RPI 4", fill="white")
         parsed_cpu_util = str(get_cpu_util_percent())
@@ -164,38 +165,40 @@ def update_oled_screen():
 
         draw.text((OLED_LEFT_PADDING, 22), "-------------------", fill="white")
 
-        if time.time() - prev_weather_check_time > WEATHER_CHECK_DELAY_SEC or got_weather_data == False:
+        if time.time() - g_prev_weather_check_time > WEATHER_CHECK_DELAY_SEC or g_got_weather_data == False:
 
             nw_conn_retry_tmp = 0
-            weather_data = ["OWM", "ERR", ":(", "_", "_"]
+            g_weather_data = ["OWM", "ERR", ":(", "_", "_"]
             while nw_conn_retry_tmp < WAIT_FOR_INTERNET_RETRY:
                 nw_conn_retry_tmp += 1
                 try:
                     init_modules()
-                    weather_data = get_weather_data()
-                    got_weather_data = True
+                    g_weather_data = get_weather_data()
+                    g_got_weather_data = True
                 except:
                     time.sleep(1) # retry after 1 sec
                     continue
                 break
 
-            prev_weather_check_time = time.time() 
+            g_prev_weather_check_time = time.time() 
 
         try:
-            draw.text((OLED_LEFT_PADDING, 30), "TMP:" + limit_str_size(weather_data[0]) + " CLD:" + weather_data[1], fill="white")
-            draw.text((OLED_LEFT_PADDING, 40), "HMD:" + weather_data[3] + " RN:" + weather_data[4], fill="white")
-            draw.text((OLED_LEFT_PADDING, 50), "STS:" + weather_data[2], fill="white")
+            draw.text((OLED_LEFT_PADDING, 30), "TMP:" + limit_str_size(g_weather_data[0]) + " CLD:" + g_weather_data[1], fill="white")
+            draw.text((OLED_LEFT_PADDING, 40), "HMD:" + g_weather_data[3] + " RN:" + g_weather_data[4], fill="white")
+            draw.text((OLED_LEFT_PADDING, 50), "STS:" + g_weather_data[2], fill="white")
         except:
             pass
 
 def cpu_fan_control():
     
-    global g_cur_temp
+    global g_cur_temp, g_cpu_fan_on
     try:
-        if g_cur_temp >= CPU_FAN_TURN_ON_THRESH:
+        if g_cur_temp >= CPU_FAN_TURN_ON_THRESH and not g_cpu_fan_on:
             GPIO.output(CPU_FAN_SIGNAL_GPIO, True)
-        elif g_cur_temp <= CPU_FAN_TURN_OFF_THRESH:
+            g_cpu_fan_on = True
+        elif g_cur_temp <= CPU_FAN_TURN_OFF_THRESH and g_cpu_fan_on:
             GPIO.output(CPU_FAN_SIGNAL_GPIO, False)
+            g_cpu_fan_on = False
     except KeyboardInterrupt:
         GPIO.cleanup()
 
@@ -219,5 +222,5 @@ if __name__ == "__main__":
 
     while True:
         update_oled_screen()
-        cpu_fan_control()
-        time.sleep(1 - 0.4) ## Update every 1 sec, CPU temp calc function takes aprox 400 ms
+        cpu_fan_control()       ## Control CPU cooling fan based on temperature
+        time.sleep(1 - 0.4)     ## Update every 1 sec, CPU temp calc function takes aprox 400 ms
